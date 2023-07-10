d478c2372460aa53f621cadcfe4752b25d08b378
gustavocarlim <gustavocarlimsaens@gmail.com>
2023-07-10 12:28:11 -0300

    creating the header

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import './Home.style.css'
+
+const Home = () => {
+    return (
+      <header className='header'>
+          <h1>React Crud Apication</h1>
+      </header>
+      )
+}
+
+export default Home;
